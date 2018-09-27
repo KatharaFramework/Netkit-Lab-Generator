41d181265a8c1a67ff805d0fb12cc94a7b239a60
Pastello <Pastello@users.noreply.github.com>
2018-09-27 13:07:02 +0200

    Tolta barra menu

--- electronClient.js
+++ electronClient.js
@@ -8,8 +8,6 @@ const fs = require('fs')
 /* -------------------------- SETUP -------------------------- */
 /* ----------------------------------------------------------- */
 
-process.env.NODE_ENV = 'production'
-
 app.on('ready', function () {
 	let mainWindow = new BrowserWindow({ minWidth: 770, minHeight: 500 })
 
@@ -19,55 +17,12 @@ app.on('ready', function () {
 		slashes: true
 	}))
 
+	mainWindow.setMenu(null)
+
 	mainWindow.on('closed', function () {
 		app.quit()
 	})
-
-	if (process.env.NODE_ENV !== 'production') {
-		const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
-		Menu.setApplicationMenu(mainMenu)
-	}
 })
-
-const mainMenuTemplate = [
-	// Each object is a dropdown
-	/*{
-	  label: 'File',
-	  submenu:[
-		{
-		  label: 'Quit',
-		  accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-		  click(){
-			app.quit()
-		  }
-		}
-	  ]
-	}*/
-]
-
-// If OSX, add empty object to menu
-if (process.platform == 'darwin') {
-	mainMenuTemplate.unshift({})
-}
-
-// Add developer tools option if in dev
-if (process.env.NODE_ENV !== 'production') {
-	mainMenuTemplate.push({
-		label: 'Developer Tools',
-		submenu: [
-			{
-				role: 'reload'
-			},
-			{
-				label: 'Toggle DevTools',
-				accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-				click(item, focusedWindow) {
-					focusedWindow.toggleDevTools()
-				}
-			}
-		]
-	})
-}
 
 /* ---------------------------------------------------------- */
 /* ------------------------- EVENTS ------------------------- */
@@ -110,9 +65,8 @@ ipcMain.on('script:clean', function () {
 })
 
 /* --------------------------- SDN --------------------------- */
-// TODO: Anziché usare i comandi docker si potrebbe usare il comando vstart
 // TODO: Controllare che funzioni anche su windows
-
+// TODO: Anziché usare i comandi docker si potrebbe usare il comando vstart
 
 ipcMain.on('sdn:connect', function () {
 	let prefix = 'netkit_$(id -u)_'
