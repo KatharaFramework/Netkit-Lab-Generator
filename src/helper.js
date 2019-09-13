b522f87ba221f5ecd346a017ebab29d3e2bad77c
Pastello <Pastello@users.noreply.github.com>
2019-09-13 22:54:57 +0200

    Indentazione e simili

--- src/helper.js
+++ src/helper.js
@@ -1,46 +1,47 @@
 function lastElem(arr) {
-    return arr[arr.length - 1]
+	return arr[arr.length - 1]
 }
 
-function toggle_tab(_, selectedTab) {
-    toggle_submenu(-1)
-    for (let elem of document.querySelectorAll(".tab-pane")) {
-        elem.classList.remove("active")
-    }
-    for (let elem of document.querySelectorAll(".tab-label")) {
-        elem.classList.remove("active")
-    }
-
-    let href = selectedTab.getAttribute("href").replace("#", '')
-    let currentTab = document.getElementById(href)
-    currentTab.classList.add("active")
-    
-    let rightControls = document.getElementById("left-controls")
-    if (href == "home") {
-        rightControls.classList.remove("ng-hide")
-    } else {
-        rightControls.classList.add("ng-hide")
-    }
+function toggle_tab(_, clickedNavTab) {
+	toggle_submenu(-1)
+	for (let elem of document.querySelectorAll(".tab-pane")) {
+		elem.classList.remove("active")
+	}
+	for (let elem of document.querySelectorAll(".tab-label")) {
+		elem.classList.remove("active")
+	}
+	
+	let href = clickedNavTab.getAttribute("href").replace("#", '')
+	
+	let currentTab = document.getElementById(href)
+	currentTab.classList.add("active")
+	
+	let rightControls = document.getElementById("left-controls")
+	if (href == "home") {
+		rightControls.classList.remove("ng-hide")
+	} else {
+		rightControls.classList.add("ng-hide")
+	}
 }
 
 function toggle_submenu(number, total = 3) {
-    let mock_main_menu = document.getElementById("mock-main-menu")
-    let hidden = false
-    for (let i = 0; i < total; i++) {
-        let current_submenu = document.getElementById("submenu-" + i)
-        if (i == number) {
-            current_submenu.classList.toggle("ng-hide")
-            if(current_submenu.classList.contains("ng-hide")) hidden = true
-        } else {
-            current_submenu.classList.add("ng-hide")
-        }
-    }
+	let mock_main_menu = document.getElementById("mock-main-menu")
+	let hidden = false
+	for (let i = 0; i < total; i++) {
+		let current_submenu = document.getElementById("submenu-" + i)
+		if (i == number) {
+			current_submenu.classList.toggle("ng-hide")
+			if(current_submenu.classList.contains("ng-hide")) hidden = true
+		} else {
+			current_submenu.classList.add("ng-hide")
+		}
+	}
 
-    if (hidden || number == -1) {
-        mock_main_menu.classList.remove("mock-submenu")
-    } else {
-        mock_main_menu.classList.add("mock-submenu")
-    }
+	if (hidden || number == -1) {
+		mock_main_menu.classList.remove("mock-submenu")
+	} else {
+		mock_main_menu.classList.add("mock-submenu")
+	}
 }
 
 function close_modal(id) {
@@ -48,7 +49,7 @@ function close_modal(id) {
 }
 
 function isElectron() {
-    return window && window.process && window.process.type
+	return window && window.process && window.process.type
 }
 
 function copyLab(){
@@ -87,7 +88,7 @@ function executeClean(e) {
 }
 
 function executeGeneric(e, command){
-    e.preventDefault()
+	e.preventDefault()
 	let modal = document.getElementById("command-modal")
 	modal.classList.remove("ng-hide")
 	setTimeout(
